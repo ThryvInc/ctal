f82eeabd1a9ebae6e079d4a7f35409201f2896fe
Elliot Schrock <elliot.schrock@gmail.com>
2013-05-28 22:01:26 -0400

    Init

---
+++ src/com/rndapp/chicagol/Notification.java
@@ -0,0 +1,15 @@
+package com.rndapp.chicagol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Notification extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+		
+	}
+
+}
